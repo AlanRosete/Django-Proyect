67c6cd120b0ef5acee231b504ed8e96bf6808cbd
AlanRosete <78501259+AlanRosete@users.noreply.github.com>
2023-09-12 16:52:11 -0600

    Cambios Vol3

--- herencia/comentarios/views.py
+++ herencia/comentarios/views.py
@@ -6,8 +6,14 @@ def test(request):
     return HttpResponse("Funciona Correctamente")
 
 def create(request):
+
     comment = Comment(name="JuanGa", score=5, comments="Prueba Comentario")
     comment.save()
+
+    #Otro metodo para realizar el objeto Class seria: 
+    
+    # comment = Comment.objects.create(name="JuanGa", score=5, comments="Prueba Comentario")
+
     return HttpResponse("Ruta para probar la creacion de modelos")
 
 # Create your views here.
